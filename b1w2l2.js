f6bb7281a50ed283f09d103c9e7a236aeb8e0f48
Daphne Kramer <99046027@mydavinci.nl>
2019-09-10 18:45:02 +0200

    Week 2
    Lab 2

---
+++ b1w2l2.js
@@ -0,0 +1,16 @@
+// begin hier je JavaScript commandos
+   // Daphne Kramer
+   // Applicatie ontwikkelaar
+      
+   const vraag = alert("Je moet 3 vragen beantwoorden");
+
+   var voornaam = prompt("Vul je voornaam in.");
+   document.write('Mijn naam is ' + voornaam + "<br/>");
+
+   var achternaam = prompt("Vul je achternaam in.");
+   document.write(' Mijn achternaam is ' + achternaam + "<br/>");
+
+   var leeftijd = prompt('Vul je leeftijd in.');
+   document.write(' Mijn leeftijd is ' + leeftijd + " jaar");
+
+
