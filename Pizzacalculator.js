2c5df166552f5ec406cee173ea29d6b410a3fa64
Daphne Kramer <99046027@mydavinci.nl>
2019-09-11 09:26:52 +0200

    Pizza calculator

--- Pizzacalculator.js
+++ Pizzacalculator.js
@@ -1,10 +1,12 @@
-
-
 alert("Kies uw pizza afmeting en het aantal"); //geeft pop-up met de vraag
 
 var Small = prompt("Small: €8,95. Hoe veel wilt u er?"); //geeft pop-up met input met de vraag
 var Medium = prompt("Medium: €10,95. Hoe veel wilt u er?"); //geeft pop-up met input met de vraag
 var Large = prompt("Large: €12,95. Hoe veel wilt u er?"); //geeft pop-up met input met de vraag
+
+var intgetal = parseInt(Small);
+var intgetal = parseInt(Medium);
+var intgetal = parseInt(Large);
 
 var priceSmall = 8.95; //de prijs van een small pizza
 var priceMedium = 10.95; //de prijs van een medium pizza
@@ -12,42 +14,21 @@ var priceLarge = 12.95; //de prijs van een large pizza
 
 
 totaal = 0;
+totaalSmall = 0;
+totaalMedium = 0;
+totaalLarge = 0;
+
 function order(){
-    //geeft het totaal van 1 small pizza
-    if (Small == 1 && Medium == 0 && Large == 0){
-        totaal = totaal + priceSmall;
-        return totaal;}
-
-    //geeft het totaal van 1 medium pizza
-    if (Small == 0 && Medium == 1 && Large == 0){
-        totaal = totaal + priceMedium;
-        return totaal;}
-
-    //geeft het totaal van 1 large pizza
-    if (Small == 0 && Medium == 0 && Large == 1){
-        totaal = totaal + priceLarge;
-        return totaal;}
-
-
-
-    if (Small == 1 && Medium == 1 && Large == 1){
-        totaal = totaal + priceSmall + priceMedium + priceLarge;
-        return totaal;
-
-    }
-    if (Small == 2 && Medium == 1 && Large == 1){
-        totaal = totaal + priceSmall*2 + priceMedium + priceLarge;
-        return totaal;
-    }
-    if (Small == 2 && Medium == 2 && Large == 1){
-        totaal = totaal + priceSmall*2 + priceMedium*2 + priceLarge;
-        return totaal;
-    }
+    totaalSmall = totaalSmall + Small * priceSmall;
+    totaalMedium = totaalMedium + Medium * priceMedium;
+    totaalLarge = totaalLarge + Large * priceLarge;
+    totaal = totaalSmall + totaalMedium + totaalLarge;
+    return totaal;
 }
 
 
 document.write("Aantal small pizza's: "+ Small + "<br/>");
 document.write("Aantal medium pizza's: " + Medium + "<br/>");
-document.write("Aantal large pizza's: " + Large + "<br/><br/>")
-document.write("Het totaal is: €" + order(totaal) );
+document.write("Aantal large pizza's: " + Large + "<br/><br/> ")
 
+document.write("Het totaal is: €" + order(totaal) );
