4aebd8bdbd609ca431fa9a89ce9da71b192da7d3
Daphne Kramer <99046027@mydavinci.nl>
2019-09-12 10:50:57 +0200

    Pizza calculator

--- Pizza calculator/assets/js/Pizzacalculator.js
+++ Pizza calculator/assets/js/Pizzacalculator.js
@@ -4,11 +4,22 @@
 
 alert("Kies uw pizza afmeting en het aantal"); //geeft pop-up met de vraag
 
-////geeft pop-up met input met de vragen hieronder
-var Small = prompt("Small: €8,95. Hoe veel wilt u er?"); 
-var Medium = prompt("Medium: €10,95. Hoe veel wilt u er?"); 
-var Large = prompt("Large: €12,95. Hoe veel wilt u er?"); 
 
+////geeft pop-up met input met de vragen hieronder
+do  {
+        small = parseInt(prompt("Small: €8,00. Hoe veel wilt u er?"));
+}   while(!isNumber(small));
+do  {
+    medium = parseInt(prompt("Medium: €10,00. Hoe veel wilt u er?"));
+}   while(!isNumber(medium));
+do  {  
+    large = parseInt(prompt("Large: €12,00. Hoe veel wilt u er?")); 
+}   while(!isNumber(large));
+
+function isNumber(value) {
+    var numberPattern = /^[0-9]+$/; // one or more of digits 0 to 9
+    return numberPattern.test(value);
+    }
 //de prijs van een small, medium en large pizza
 var priceSmall = 8.00;
 var priceMedium = 10.00;
@@ -23,9 +34,9 @@ totaalLarge = 0.00;
 
 //de function telt de prijzen en het aantal van de pizza bij elkaar op en retunt het hele totaal hiervan
 function order(){
-    totaalSmall = totaalSmall + Small * priceSmall;
-    totaalMedium = totaalMedium + Medium * priceMedium;
-    totaalLarge = totaalLarge + Large * priceLarge;
+    totaalSmall = totaalSmall + small * priceSmall;
+    totaalMedium = totaalMedium + medium * priceMedium;
+    totaalLarge = totaalLarge + large * priceLarge;
     totaal = totaalSmall + totaalMedium + totaalLarge;
     return totaal;
 }
@@ -33,8 +44,8 @@ function order(){
 
 
 //hier worden het aantal pizza's mee op het scherm getoond.
-document.write("Aantal small pizza's: "+ Small + "<br/>");
-document.write("Aantal medium pizza's: " + Medium + "<br/>");
-document.write("Aantal large pizza's: " + Large + "<br/><br/> ")
+document.write("Aantal small pizza's: "+ small + "<br/>");
+document.write("Aantal medium pizza's: " + medium + "<br/>");
+document.write("Aantal large pizza's: " + large + "<br/><br/> ")
 
-document.write("Het totaal is: €" + order(totaal)+",-" ); //roept uit de function 'order' het totaal op van alle prijzen bij elkaar opgeteld en toont het op het scherm
+document.write("Het totaal is: €" + order(totaal).toFixed(2)); //roept uit de function 'order' het totaal op van alle prijzen bij elkaar opgeteld en toont het op het scherm
